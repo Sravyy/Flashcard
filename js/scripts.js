05a435c47f4d7ac0759b318d0fb01de2aa6a6ec5
john murray and sravanthi velaga <john@>
2017-09-12 11:46:54 -0700

    Add HTML Content , styling and Script

---
+++ js/scripts.js
@@ -0,0 +1,75 @@
+$(function(){
+  $(".js1").click(function() {
+    $(".javascript").toggle();
+    $("javascript-head").toggle();
+  });
+  $(".operators1").click(function() {
+    $(".operators").toggle();
+    $("operators-head").toggle();
+  });
+  $(".variables1").click(function() {
+    $(".variables").toggle();
+    $("variables-head").toggle();
+  });
+  $(".function").click(function() {
+    $(".functions").toggle();
+    $("functions-head").toggle();
+  });
+  $(".vnc1").click(function() {
+    $(".vnc").toggle();
+    $("vnc-head").toggle();
+  });
+  $(".para1").click(function() {
+    $(".para").toggle();
+    $("para-head").toggle();
+  });
+  $(".arguments").click(function() {
+    $(".argue").toggle();
+    $("argue-head").toggle();
+  });
+  $(".return1").click(function() {
+    $(".return").toggle();
+    $("return-head").toggle();
+  });
+  $(".cm").click(function() {
+    $(".chain").toggle();
+    $("chain-head").toggle();
+  });
+  $(".strings1").click(function() {
+    $(".strings").toggle();
+    $("strings-head").toggle();
+  });
+  $(".boolean").click(function() {
+    $(".bool").toggle();
+    $("bool-head").toggle();
+  });
+  $(".undefined1").click(function() {
+    $(".undefined").toggle();
+    $("undefined-head").toggle();
+  });
+  $(".nan1").click(function() {
+    $(".nan").toggle();
+    $("nan-head").toggle();
+  });
+  $(".escape").click(function() {
+    $(".esc").toggle();
+    $("esc-head").toggle();
+  });
+  $(".alert1").click(function() {
+    $(".alert").toggle();
+    $("alert-head").toggle();
+  });
+  $(".comments").click(function() {
+    $(".com").toggle();
+    $("com-head").toggle();
+  });
+  $(".jquery").click(function() {
+    $(".jq").toggle();
+    $("jq-head").toggle();
+  });
+  $(".attributes").click(function() {
+    $(".attr").toggle();
+    $("attr-head").toggle();
+  });
+
+});
